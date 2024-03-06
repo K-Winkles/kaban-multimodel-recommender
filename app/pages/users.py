98e67cd177710e7cb2ea5b68b816d65de64de3bf
julius.cris.salinas <julius.cris.salinas@gmail.com>
2024-03-07 00:24:23 +0800

    add user history to pickle file export add banner, fix app repoint source

--- app/pages/users.py
+++ app/pages/users.py
@@ -22,25 +22,13 @@ def get_csv_files(directory):
                 csv_files.append(os.path.join(root, file))
     return csv_files
 
-def get_asin_name_mapping():
-    files = get_csv_files(folder_path)
-    df_itemset = pd.DataFrame()
-    for file in files:
-        try:
-            df = pd.read_csv(file, low_memory=False)[['name','asin','image', 'price','customerReview']]
-            df_itemset = pd.concat([df_itemset, df], ignore_index=True)   
-        except Exception as e:
-            print(e)
-    df_itemset['asin'] = df_itemset['asin'].str.lower()
-    df_itemset = df_itemset.drop_duplicates(subset='asin', keep='first')
-    return df_itemset
-
 st.set_page_config(page_title="KabanMarket.com")
-df_asin_mapping = get_asin_name_mapping()
-    
+st.image('app/pages/image.png')
+df_asin_mapping = pd.read_csv('dataset/utility/reviews.csv')
 # Display Top Reviewers
-user_names = user_hist_reco_dict[next(iter(user_hist_reco_dict.keys()))].keys()
-selected_user = st.selectbox("Select a Reviewer", [name.replace('_', '') for name in user_names])
+user_names = list(user_hist_reco_dict.keys())
+selected_user = st.selectbox("Select User:", user_names)
+
 # Function to display buttons
 def display_buttons(asin_value, algo, prodtype):
     # Container to hold the buttons
@@ -60,8 +48,10 @@ def main_page():
     # Create tabs dynamically based on the keys of the dictionary
     with st.expander("User history"):
                 st.title(selected_user.title() + "'s Reviewed products:")
-                df_user_hist = user_hist_reco_dict['SVD'][selected_user+'_'][0].reset_index(drop=True)
-                df_user_hist = pd.merge(df_user_hist, df_asin_mapping, left_on='ASIN', right_on='asin', how='left')
+                df_user_hist = user_hist_reco_dict[selected_user]['SVD'][0].reset_index(drop=True)
+                df_user_hist = df_user_hist.drop('ProductName', axis=1)
+                df_user_hist = pd.merge(df_user_hist, df_asin_mapping, on='ASIN')
+                df_user_hist = df_user_hist.drop_duplicates(subset='ASIN', keep='first')
                 # Display items as small tiles in three columns
                 cols = st.columns(3)  # Create three columns
                 for index, item in df_user_hist.iterrows():
@@ -69,23 +59,23 @@ def main_page():
                     with col:
                         try:
                             st.image(item["image"], use_column_width=True)
-                            name = item["name"]
+                            name = item["ProductName"].replace('_',' ').title()
                             st.markdown(f"<b>{name}</b></div>", unsafe_allow_html=True)
-                            st.markdown(f"<span style='color: red;font-size: 20px;'>{item['price']}</span>", unsafe_allow_html=True)
-                            st.caption(item['customerReview'])
-                            asin_value = item['asin']
+                            st.markdown(f"<span style='color: red;font-size: 20px;'>$ {item['price']}</span>", unsafe_allow_html=True)
+                            asin_value = item['ASIN']
                             display_buttons(asin_value,'hist','hist')
                             st.divider()
                         except Exception as e:
                             print(e)
-    tabs = st.tabs(list(user_hist_reco_dict.keys()))
+    tabs = st.tabs(list(user_hist_reco_dict[next(iter(user_hist_reco_dict))].keys()))
     # Iterate over each tab and key in the dictionary simultaneously
-    for tab, algo in zip(tabs, user_hist_reco_dict.keys()):
+    for tab, algo in zip(tabs, user_hist_reco_dict[next(iter(user_hist_reco_dict))].keys()):
         with tab:
             with st.expander("Recommended items", expanded = True):
-                st.title(selected_user.title() + "'s Recommended Products:")
-                df_reco = user_hist_reco_dict[algo][selected_user+'_'][1]
-                df_reco = pd.merge(df_reco, df_asin_mapping, left_on='ASIN', right_on='asin', how='left')
+                st.title(selected_user.split('_')[0].title() + "'s Recommended Products:")
+                df_reco = pd.DataFrame(user_hist_reco_dict[selected_user][algo][1], columns=['ASIN'])
+                df_reco = pd.merge(df_reco, df_asin_mapping, on = 'ASIN')
+                df_reco = df_reco.drop_duplicates(subset='ASIN', keep='first')
                 # Display items as small tiles in three columns
                 cols = st.columns(3)  # Create three columns
                 for index, item in df_reco.iterrows():
@@ -93,11 +83,10 @@ def main_page():
                     with col:
                         try:
                             st.image(item["image"], use_column_width=True)
-                            name = item["name_x"]
+                            name = item["ProductName"].replace('_',' ').title()
                             st.markdown(f"<b>{name}</b></div>", unsafe_allow_html=True)
-                            st.markdown(f"<span style='color: red;font-size: 20px;'>{item['price']}</span>", unsafe_allow_html=True)
-                            st.caption(item['customerReview'])
-                            asin_value = item['asin']
+                            st.markdown(f"<span style='color: red;font-size: 20px;'>$ {item['price']}</span>", unsafe_allow_html=True)
+                            asin_value = item['ASIN']
                             display_buttons(asin_value,algo,'reco')
                             st.divider()
                         except Exception as e:
